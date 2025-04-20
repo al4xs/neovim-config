3d0b01b8298d9c4dc3942bccae3f24c2946f3c53
al4xs <code.allanjose@gmail.com>
2025-04-19 23:29:24 -0500

    feat: add auto-save plugin and FiraCode font install script

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,23 @@
+return {
+  "Pocco81/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" },
+  config = function()
+    require("auto-save").setup({
+      enabled = true,
+      execution_message = {
+        message = function() return "" end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      trigger_events = {"InsertLeave", "TextChanged"},
+      condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+          return true
+        end
+        return false
+      end,
+    })
+  end
+}
